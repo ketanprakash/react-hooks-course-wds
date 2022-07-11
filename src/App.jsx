fd563ffa786cb04fbfb01d79f7bc2574a49a1479
ketanprakash <ketanprakash001@gmail.com>
2022-07-11 16:17:24 +0530

    useState done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const App = () => {
+  const [count, setCount] = useState(() => 4);
+
+  const decrementCount = () => {
+    setCount(prevCount => prevCount - 1);
+  }
+
+  const incrementCount = () => {
+    setCount(prevCount => prevCount + 1)
+  }
   return (
-    <div className="App">
-      <h1>Homepage</h1>
-    </div>
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>&nbsp;&nbsp;{count}&nbsp;&nbsp;</span>
+      <button onClick={incrementCount}>+</button>
+    </>
   )
 }
 
