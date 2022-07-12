2262ebb67fa1087d7efc6d62b6f18285a467fa58
ketanprakash <ketanprakash001@gmail.com>
2022-07-12 17:32:07 +0530

    useReducer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useReducer } from "react";
+
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+}
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case ACTIONS.INCREMENT:
+      return {...state, count: state.count + 1};
+    case ACTIONS.DECREMENT:
+      return {...state, count: state.count - 1};
+    default:
+      return state;
+  }
+}
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, {count: 0});
+
+  const increment = () => {
+    dispatch({type: ACTIONS.INCREMENT});
+  }
+
+  const decrement = () => {
+    dispatch({type: ACTIONS.DECREMENT})
+  }
+  
   return (
-    <div className="App">
-      <h1>Homepage</h1>
+    <div>
+      <button onClick={increment}>+</button>
+      <span>&nbsp;{state.count}&nbsp;</span>
+      <button onClick={decrement}>-</button>
     </div>
   )
 }
