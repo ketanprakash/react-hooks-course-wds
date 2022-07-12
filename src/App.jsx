acaf492aef7279cf6f2e0f920bdca5e5a1e3e2d8
ketanprakash <ketanprakash001@gmail.com>
2022-07-12 19:49:56 +0530

    useTransition done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { useState, useTransition } from "react";
+
 const App = () => {
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  const [isPending, startTransition] = useTransition();
+
+  const LIST_SIZE = 2000;
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    startTransition(() => {
+      const l = [];
+      for (let i = 0; i < LIST_SIZE; i++){
+        l.push(e.target.value);
+      }
+      setList(l);
+    });
+  } 
+
   return (
-    <div className="App">
-      <h1>Homepage</h1>
-    </div>
+    <>
+      <input type="text" value={input} onChange={handleChange}/>
+      {isPending ? <div>Loading...</div>: list.map((item, index) => {
+        return <div key={index}>{item}</div>
+      })}
+    </>
   )
 }
 
