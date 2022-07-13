06ce0a10c0dcab8801fdd7654feed9b9721486fe
ketanprakash <ketanprakash001@gmail.com>
2022-07-13 14:45:28 +0530

    made useEffect remount proof(react18 useEffect problem) and added key to items

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,63 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
-// const App = () => {
-//   const [resourceType, setResourceType] = useState('posts');
-//   const [items, setItems] = useState([])
-//   useEffect(() => {
-//     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-//       .then(response => response.json())
-//       .then(data => setItems(data));
-//   }, [resourceType])
-
-//   return (
-//     <>
-//       <div>
-//         <button onClick={() => setResourceType('posts')}>Posts</button>
-//         <button onClick={() => setResourceType('users')}>Users</button>
-//         <button onClick={() => setResourceType('comments')}>Comments</button>
-//       </div>
-//       <h1
-//         style={{
-//           textTransform: 'capitalize'
-//         }}
-//       >{resourceType}</h1>
-//       {items.map((item) => (
-//         <div>{JSON.stringify(item)}</div>
-//       ))}
-//     </>
-//   )
-// }
-
-function App(){
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  const handleResize = () => {
-    setWindowWidth(window.innerWidth);
-  }
-
+const App = () => {
+  const [resourceType, setResourceType] = useState('posts');
+  const [items, setItems] = useState([])
+  const fetchedResource = useRef('');
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
+    if (fetchedResource.current !== resourceType){
+      fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+        .then(response => response.json())
+        .then(data => {
+          setItems(data);
+        });
+        fetchedResource.current = resourceType;
     }
-  }, [])
+  }, [resourceType])
 
   return (
-    <div 
-      className="App"
-      style={{
-        fontSize: "2rem"
-      }}
-    >
-      {windowWidth}
-    </div>
+    <>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
+      </div>
+      <h1
+        style={{
+          textTransform: 'capitalize'
+        }}
+      >{resourceType}</h1>
+      {items.map((item) => (
+        <div key={`item${item.id}`}>{JSON.stringify(item)}</div>
+      ))}
+    </>
   )
 }
+
+// function App(){
+//   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+//   const handleResize = () => {
+//     setWindowWidth(window.innerWidth);
+//   }
+
+//   useEffect(() => {
+//     window.addEventListener('resize', handleResize);
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     }
+//   }, [])
+
+//   return (
+//     <div 
+//       className="App"
+//       style={{
+//         fontSize: "2rem"
+//       }}
+//     >
+//       {windowWidth}
+//     </div>
+//   )
+// }
 
 export default App;
